ece22d4bb96da7b41da01366abdefb50e0f74abe
Marcus Holmgren <marcus.holmgren1@gmail.com>
2016-10-02 15:13:09 +0200

    Test complicated rolls.
    Test strike, spare and perfect game

--- BowlingTest.py
+++ BowlingTest.py
@@ -15,13 +15,29 @@ class BowlingTest(unittest.TestCase):
         self.roll_many(20, 1)
         self.assertEqual(20, self.sut.score())
 
-    @unittest.skip('refactor calculcation')
     def test_one_spare(self):
-        self.sut.roll(5)
-        self.sut.roll(5)  # spare
+        self.roll_spare()
         self.sut.roll(3)
         self.roll_many(17, 0)
         self.assertEqual(16, self.sut.score())
+
+    def test_one_strike(self):
+        self.roll_strike()
+        self.sut.roll(3)
+        self.sut.roll(4)
+        self.roll_many(16, 0)
+        self.assertEqual(24, self.sut.score())
+
+    def test_perfect_game(self):
+        self.roll_many(12, 10)
+        self.assertEqual(300, self.sut.score())
+
+    def roll_strike(self):
+        self.sut.roll(10)
+
+    def roll_spare(self):
+        self.sut.roll(5)
+        self.sut.roll(5)
 
     def roll_many(self, n, pins):
         for i in range(n):
--- Game.py
+++ Game.py
@@ -1,9 +1,42 @@
 class Game(object):
     def __init__(self):
-        self.current_score = 0
+        self.rolls = []
+        self.current_roll = 0
 
     def roll(self, pins):
-        self.current_score = self.current_score + pins
+        self.rolls.append(pins)
+        self.current_roll += 1
 
     def score(self):
-        return self.current_score
+        total_score = 0
+        first_in_frame = 0
+        for frame in range(10):
+            if self.is_strike(first_in_frame):
+                total_score += 10 + self.next_two_balls_for_strike(first_in_frame)
+                first_in_frame += 1
+            elif self.is_spare(first_in_frame):
+                total_score += 10 + self.next_ball_for_spare(first_in_frame)
+                first_in_frame += 2
+            elif first_in_frame + 1 < len(self.rolls):
+                total_score += self.two_balls_in_frame(first_in_frame)
+                first_in_frame += 2
+        return total_score
+
+    def is_spare(self, first_in_frame):
+        if first_in_frame + 1 < len(self.rolls):
+            return self.two_balls_in_frame(first_in_frame) == 10
+        return False
+
+    def two_balls_in_frame(self, first_in_frame):
+        return self.rolls[first_in_frame] + self.rolls[first_in_frame + 1]
+
+    def is_strike(self, first_in_frame):
+        if first_in_frame + 1 < len(self.rolls):
+            return self.rolls[first_in_frame] == 10
+        return False
+
+    def next_two_balls_for_strike(self, first_in_frame):
+        return self.rolls[first_in_frame + 1] + self.rolls[first_in_frame + 2]
+
+    def next_ball_for_spare(self, first_in_frame):
+        return self.rolls[first_in_frame + 2]
